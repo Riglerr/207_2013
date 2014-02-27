625f0135cf768bcc65646338664475493d402f3e
James Shuttleworth <csx239@coventry.ac.uk>
2014-02-27 11:44:31 +0000

    Extended

--- week19/makearray.c
+++ week19/makearray.c
@@ -1,9 +1,14 @@
 int main(int argc, char *argv[])
 {
+  int count=0;
   int i;
   int* a=(int*) malloc(20*sizeof(int));
   for(i=0;i<=20;i++){
+    count=(count+1)%3;
     a[i]=0;
+    if(a[i]=0){
+      printf("BING!\n");
+    }
   }
   return 0;
 }
